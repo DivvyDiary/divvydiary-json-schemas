8b0e36605b6a255b417910acf774972c8a6930dd
Johannes Kronmüller <johannes.kronmueller@gmail.com>
2020-07-04 20:36:07 +0200

    first draft

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const defs = require("./schemas/defs.json");
+const dividend = require("./schemas/dividend.json");
+const symbol = require("./schemas/symbol.json");
+const user = require("./schemas/user.json");
+
+module.exports = {
+  defs,
+  dividend,
+  symbol,
+  user,
+};
---
+++ src/test.js
@@ -0,0 +1,13 @@
+const Ajv = require("ajv");
+const glob = require("glob");
+const path = require("path");
+
+var ajv = new Ajv();
+
+glob.sync(`${__dirname}/schemas/*.json`).forEach((file) => {
+  console.log(file);
+
+  const schema = require(path.resolve(file));
+
+  ajv.compile(schema);
+});
