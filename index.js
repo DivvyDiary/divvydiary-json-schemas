09d7d7faa1c9758a319182103e0ed87e75ae54bd
Johannes Kronmüller <johannes.kronmueller@gmail.com>
2020-07-05 18:19:41 +0200

    feat: blog article schema

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const article = require("./src/schemas/article.json");
 const defs = require("./src/schemas/defs.json");
 const dividend = require("./src/schemas/dividend.json");
 const symbol = require("./src/schemas/symbol.json");
 const user = require("./src/schemas/user.json");
 
 module.exports = {
+  article,
   defs,
   dividend,
   symbol,
