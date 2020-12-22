0062528eb305cfd5df9703a2ecc35576c62207fc
Johannes Kronmüller <johakr@users.noreply.github.com>
2020-12-22 16:17:03 +0100

    xhore: ugprade ajv

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-const Ajv = require("ajv");
+const Ajv = require("ajv").default;
+const addFormats = require("ajv-formats").default;
+
 const glob = require("glob");
 const path = require("path");
 
 var ajv = new Ajv();
+addFormats(ajv);
 
 glob.sync(`${__dirname}/src/schemas/*.json`).forEach((file) => {
   console.log(file);
